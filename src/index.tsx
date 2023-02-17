221685cc3db4a490e95de73dd73bb95f794d4392
Alexeev Alexander <work.alexeevs@gmail.com>
2023-02-17 11:33:14 +0200

    create socket

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './api/config/fetch.config';
-import './common/styles/styles.scss';
+import './styles/styles.scss';
 import { RouterProvider } from 'react-router-dom';
 import { useCreateRouter } from './common/hooks/useCreateRouter';
 import { RootModals } from './modules/popup/components/RootModals';
+import { socket, SocketContext } from './common/utils/context/socket';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 const Routing = () => <RouterProvider router={useCreateRouter()} />;
 
 root.render(
   <>
-    <Routing />
+    <SocketContext.Provider value={socket}>
+      <Routing />
+    </SocketContext.Provider>
     <RootModals />
   </>
 );
