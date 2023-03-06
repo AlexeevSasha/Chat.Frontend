e59d52a503b29fb67368f6ca3a0ff6dfcd0ce396
work.alexeevs@gmail.com <work.alexeevs@gmail.com>
2023-03-06 15:21:57 +0200

    socket

--- src/common/hooks/useSocket.tsx
+++ src/common/hooks/useSocket.tsx
@@ -1,14 +1,15 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useUserStore } from '../../modules/user/store';
 import { connectSocket } from '../utils/connectSocket';
+import { Socket } from 'socket.io-client';
 
 export const useSocket = () => {
+  const [socket, setSocket] = useState<Socket>();
   const id = useUserStore((state) => state.user?.id);
-  const socket = connectSocket();
 
   useEffect(() => {
-    if (id) socket.connect();
-    else socket.disconnect();
+    if (id) setSocket(connectSocket());
+    else socket?.disconnect();
   }, [id]);
 
   return { socket };
